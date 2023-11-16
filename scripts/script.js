805f386731d432bdb63fd7c338c6fbcdf332926f
Kenia Montalvo <kenlimonra@gmail.com>
2023-11-15 20:53:03 -0600

    se hizo responsive

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form");
+const nombre = document.getElementById("name");
+const correo = document.getElementById("email");
+const asunto = document.getElementById("topic");
+const mensaje = document.getElementById("mensaje");
+const botonEnviar = document.getElementById("enviar-btn");
+
+const advertenciaNombre = document.getElementById("advertenciaNombre");
+const advertenciaEmail = document.getElementById("advertenciaEmail");
+const advertenciaAsunto = document.getElementById("advertenciaAsunto");
+const advertenciaMensaje = document.getElementById("advertenciaMensaje");
+const advertenciaGeneral = document.getElementById("advertenciaGeneral");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Restablecer los mensajes de advertencia
+    resetearMensajes();
+
+    if (nombre.value.length === 0) {
+        mostrarAdvertencia(advertenciaNombre, "Ingrese un nombre");
+    }
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(correo.value)) {
+        mostrarAdvertencia(advertenciaEmail, "El correo electrónico no es válido");
+    }
+    if (asunto.value.length === 0) {
+        mostrarAdvertencia(advertenciaAsunto, "Ingrese un asunto");
+    }
+    if (mensaje.value.length === 0) {
+        mostrarAdvertencia(advertenciaMensaje, "Ingrese un mensaje");
+    }
+    else {
+        advertenciaGeneral.innerHTML = "¡Tu mensaje se ha enviado!";
+    }
+});
+
+function resetearMensajes() {
+    advertenciaNombre.innerHTML = "";
+    advertenciaEmail.innerHTML = "";
+    advertenciaAsunto.innerHTML = "";
+    advertenciaMensaje.innerHTML = "";
+    advertenciaGeneral.innerHTML = "";
+}
+
+function mostrarAdvertencia(elemento, mensaje) {
+    elemento.innerHTML = mensaje;
+}
